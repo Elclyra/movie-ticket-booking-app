fd48ef28d4586287d8f23d581dd8553be9115512
Edvard Nurk <edvardnurk@gmail.com>
2024-03-05 17:57:33 +0200

    Java backend init

---
+++ CgiCinemaAppBackend/src/test/java/com/cinemaApp/CgiCinemaApp/CgiCinemaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemaApp.CgiCinemaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiCinemaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
